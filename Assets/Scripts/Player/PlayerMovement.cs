e2e1b8033e2acb65a672f5772ee6aa11a0a3e175
Zeke Abshire <zekeabshire@gmail.com>
2022-03-01 02:20:40 -0600

    Started work on movement - unsuccessful

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Components
+    private Rigidbody rb;
+    private PlayerStats stats;
+
+    // Movement
+    private float facing = 0;
+    private float modelFacing = 1;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        stats = GetComponent<PlayerStats>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+
     }
+
+    private void Update()
+    {
+        // ...
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (facing * modelFacing < 0)
+        {
+            Debug.Log("flip");
+        }
+
+        rb.AddForce(new Vector3(facing * stats.moveSpeed * Time.deltaTime, 0, 0));
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        facing = context.ReadValue<float>();
+    }
+
+    public void OnJump() { }
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float moveSpeed;
 }
