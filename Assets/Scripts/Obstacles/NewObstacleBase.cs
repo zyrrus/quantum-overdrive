77b9c20a094157ae3bed5c6867d704065304e0af
Zeke Abshire <zekeabshire@gmail.com>
2022-05-02 14:06:19 -0500

    updated obstacle base with note on future merge

---
+++ Assets/Scripts/Obstacles/NewObstacleBase.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class NewObstacleBase : MonoBehaviour
+{
+    // Keep this one and delete the other, but see what depends on the other one first
+
+    private Vector3 resetPoint;
+    [SerializeField] private bool isBouncy;
+    [SerializeField] private bool isDeadly;
+    public bool IsBouncy { get => isBouncy; private set { isBouncy = value; } }
+    public bool IsDeadly { get => isDeadly; private set { isDeadly = value; } }
+
+    private void Start() => resetPoint = gameObject.transform.position;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerCollision pc = other.gameObject.GetComponent<PlayerCollision>();
+            pc.OnHitPlayer(transform.position, true);
+        }
+    }
+
+    public void ResetObstacle() => gameObject.transform.position = resetPoint;
+}
