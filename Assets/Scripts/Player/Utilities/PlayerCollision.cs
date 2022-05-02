f84d46f078bf78d1881168cb5e362aaec994c88b
Zeke Abshire <zekeabshire@gmail.com>
2022-05-02 14:07:39 -0500

    enemy collision prototype

--- Assets/Scripts/Player/Utilities/PlayerCollision.cs
+++ Assets/Scripts/Player/Utilities/PlayerCollision.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerStateMachine psm;
+
+    private void Awake() => psm = GetComponent<PlayerStateMachine>();
+
+    public void OnHitPlayer(Vector2 obstaclePos, bool isObstacleBouncy)
     {
-        
+        Debug.Log("Player and Obstacle collision");
+        if (isObstacleBouncy && psm.IsDashing)
+            LaunchPlayer(obstaclePos);
+        else KillPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LaunchPlayer(Vector2 obstaclePos)
     {
-        
+        Debug.Log("LAUNCH");
+
+        Vector2 oldVel = psm.Rb.velocity;
+        psm.Rb.velocity = new Vector2();
+
+        psm.Rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
     }
+    private void KillPlayer() { Debug.Log("DEAD"); }
 }
